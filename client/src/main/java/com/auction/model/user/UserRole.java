338fcb5770707493f8e27a60b3fb61aff1acbefc
MinhTran <minhtran07.m@gmail.com>
2026-04-05 12:25:43 +0700

    sử dụng enum định nghĩa vai trò

--- client/src/main/java/com/auction/model/user/UserRole.java
+++ client/src/main/java/com/auction/model/user/UserRole.java
@@ -1,4 +1,11 @@
 package com.auction.model.user;
+/**
+ * Enum định nghĩa các vai trò người dùng trong hệ thống.
+ */
+public enum UserRole {
+    BIDDER("Bidder"),
+    SELLER("Seller"),
+    ADMIN("Admin");
 
 public class UserRole {
 }
