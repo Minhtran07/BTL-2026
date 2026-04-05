a20a29c3ba98c41478e0885c47c7ca5adbd4ce62
MinhTran <minhtran07.m@gmail.com>
2026-04-05 12:31:45 +0700

    admin

--- client/src/main/java/com/auction/model/user/Admin.java
+++ client/src/main/java/com/auction/model/user/Admin.java
@@ -1,4 +1,28 @@
 package com.auction.model.user;
 
-public class Admin {
+/**
+ * Lớp Admin - quản trị hệ thống.
+ * Kế thừa User - Polymorphism.
+ */
+public class Admin extends User {
+
+    private static final long serialVersionUID = 1L;
+
+    public Admin() {
+        super();
+    }
+
+    public Admin(String username, String password, String email, String fullName) {
+        super(username, password, email, fullName);
+    }
+
+    @Override
+    public UserRole getRole() {
+        return UserRole.ADMIN;
+    }
+
+    @Override
+    public String printInfo() {
+        return String.format("Admin[id=%s, username=%s]", getId(), getUsername());
+    }
 }
