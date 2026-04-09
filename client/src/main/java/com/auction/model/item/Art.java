df8392333bb8be2466f920c1c90ee97c0b90de63
NguyenDinhPhuc <phuc27062007@gmail.com>
2026-04-09 18:10:48 +0700

    sửa module Item

--- client/src/main/java/com/auction/model/item/Art.java
+++ client/src/main/java/com/auction/model/item/Art.java
@@ -2,7 +2,7 @@ package com.auction.model.item;
 
 public class Art extends Item{
 
-    private static final long   serialVersionUID = 1L;
+    private static final long  serialVersionUID = 1L;
 
     private String artist;
     private int years;
