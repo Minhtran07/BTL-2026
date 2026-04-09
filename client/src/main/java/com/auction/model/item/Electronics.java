383b7538c6891273428f26516239b7a513c551e7
NguyenDinhPhuc <phuc27062007@gmail.com>
2026-04-09 17:54:34 +0700

    sửa module Item

--- client/src/main/java/com/auction/model/item/Electronics.java
+++ client/src/main/java/com/auction/model/item/Electronics.java
@@ -1,4 +1,40 @@
 package com.auction.model.item;
 
-public class Electronics {
+public class Electronics extends Item {
+    private static final long serialVersionUID = 1L;
+
+    private String brand;
+    private String model;
+    private String condition;
+
+    public Electronics(){super();}
+    public Electronics(String name, String description,double startingPrice, String sellerId,
+                    String brand,String model,String condition){
+        super(name, description, startingPrice, sellerId);
+        this.brand = brand;
+        this.model = model;
+        this.condition = condition;
+    }
+
+    @Override
+    public ItemCategory getCategory(){
+        return ItemCategory.ELECTRONICS;
+    }
+
+    //đóng gói
+    public String getBrand(){return brand;}
+    public void setBrand(String brand){this.brand =brand;}
+
+    public String getModel(){return model;}
+    public void setModel(String model){this.model=model;}
+
+    public String getCondition(){return condition;}
+    public void setCondition(String condition){this.condition = condition;}
+
+    @Override
+    public String printInfo(){
+        return String.format("ELECTRONICS[id=%s,name=%s,brand=%s,model=%s,condition=%s,price=%.2f",
+                getId(),getName(),brand,model,condition,getStartingPrice());
+    }
+
 }
--- client/src/main/java/com/auction/model/item/Vehicle.java
+++ client/src/main/java/com/auction/model/item/Vehicle.java
@@ -1,4 +1,37 @@
 package com.auction.model.item;
 
-public class Vehicle {
+public class Vehicle extends Item{
+    private static final long serialVersionUID =1L;
+
+    private String make;
+    private String vehicleModel;
+    private int years;
+    private int mileage;
+
+    public Vehicle(){super();}
+
+    public Vehicle(String name,String description,double startingPrice,String sellerId,
+                   String make,String vehicleModel, int years, int mileage){
+        super(name, description, startingPrice, sellerId);
+        this.make = make;
+        this.vehicleModel =vehicleModel;
+        this.mileage = mileage;
+    }
+
+    @Override
+    public ItemCategory getCategory(){
+        return ItemCategory.VEHICLE;
+    }
+
+    public String getMake(){return make;}
+    public void setMake(String make){this.make = make;}
+
+    public String getVehicleModel(){return vehicleModel;}
+    public void setVehicleModel(){this.vehicleModel = vehicleModel;}
+
+    public int getYears(){return  years;}
+    public void setYears(int years){this.years=years;}
+
+    public int getMileage(){return  mileage;}
+    public void setVehicleModel(int mileage){this.mileage = mileage;}
 }
