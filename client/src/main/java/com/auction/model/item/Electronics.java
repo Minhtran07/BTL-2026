ad2a44e24bb54782ebd9dfa90eff53e5af11e41b
NguyenDinhPhuc <phuc27062007@gmail.com>
2026-04-09 18:29:46 +0700

    sửa module Item

--- client/src/main/java/com/auction/model/item/Electronics.java
+++ client/src/main/java/com/auction/model/item/Electronics.java
@@ -1,7 +1,7 @@
 package com.auction.model.item;
 
 public class Electronics extends Item {
-    private static final long serialVersionUID = 1L;
+    private static final long  serialVersionUID = 1L;
 
     private String brand;
     private String model;
