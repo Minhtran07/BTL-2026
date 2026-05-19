4732857b0b37e14e6b950152af690f9256760757
NguyenDinhPhuc <phuc27062007@gmail.com>
2026-05-19 19:27:09 +0700

    sửa lại item

--- src/main/java/com/auction/model/item/Electronics.java
+++ src/main/java/com/auction/model/item/Electronics.java
@@ -1,18 +1,23 @@
 package com.auction.model.item;
+
 /**
  * Lớp Electronics - sản phẩm điện tử.
  * Kế thừa Item - Inheritance & Polymorphism.
  */
 public class Electronics extends Item {
-    private static final long  serialVersionUID = 1L;
+
+    private static final long serialVersionUID = 1L;
 
     private String brand;
     private String model;
-    private String condition;
+    private String condition; // NEW, LIKE_NEW, USED, REFURBISHED
 
-    public Electronics(){super();}
-    public Electronics(String name, String description,double startingPrice, String sellerId,
-                    String brand,String model,String condition){
+    public Electronics() {
+        super();
+    }
+
+    public Electronics(String name, String description, double startingPrice,
+                       String sellerId, String brand, String model, String condition) {
         super(name, description, startingPrice, sellerId);
         this.brand = brand;
         this.model = model;
@@ -20,24 +25,40 @@ public class Electronics extends Item {
     }
 
     @Override
-    public ItemCategory getCategory(){
+    public ItemCategory getCategory() {
         return ItemCategory.ELECTRONICS;
     }
 
-    //đóng gói
-    public String getBrand(){return brand;}
-    public void setBrand(String brand){this.brand =brand;}
-
-    public String getModel(){return model;}
-    public void setModel(String model){this.model=model;}
-
-    public String getCondition(){return condition;}
-    public void setCondition(String condition){this.condition = condition;}
-
-    @Override
-    public String printInfo(){
-        return String.format("ELECTRONICS[id=%s,name=%s,brand=%s,model=%s,condition=%s,price=%.2f",
-                getId(),getName(),brand,model,condition,getStartingPrice());
+    public String getBrand() {
+        return brand;
     }
 
+    public void setBrand(String brand) {
+        this.brand = brand;
+        markUpdated();
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+        markUpdated();
+    }
+
+    public String getCondition() {
+        return condition;
+    }
+
+    public void setCondition(String condition) {
+        this.condition = condition;
+        markUpdated();
+    }
+
+    @Override
+    public String printInfo() {
+        return String.format("Electronics[id=%s, name=%s, brand=%s, model=%s, condition=%s, price=%.2f]",
+                getId(), getName(), brand, model, condition, getStartingPrice());
+    }
 }
